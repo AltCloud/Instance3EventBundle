95774e5030a2b38d2b3ea19a1c860e2037af38df
Jeffrey den Drijver <jeffrey@d-n-a-events.nl>
2013-04-01 23:13:44 +0200

    Update form types to sf 2.1.x

--- Form/EventType.php
+++ Form/EventType.php
@@ -3,11 +3,11 @@
 namespace AltCloud\Instance3EventBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\FormBuilder;
+use Symfony\Component\Form\FormBuilderInterface;
 
 class EventType extends AbstractType
 {
-    public function buildForm(FormBuilder $builder, array $options)
+    public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
             ->add('title')
